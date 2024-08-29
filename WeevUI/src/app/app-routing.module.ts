0839b50fc06c24913e564d2c4e44e89eaa959611
Angad Anand <angadanandishu@gmail.com>
2024-08-29 18:06:54 +0530

    commit message

---
+++ WeevUI/src/app/app-routing.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+
+import {CategorySelectionComponent} from './features/category-dashboard/category-selection/category-selection.component';
+// import { DashboardComponent } from './features/category-dashboard/dashboard/dashboard.component';
+
+
+const routes: Routes = [
+  // {
+  //   path:'Login', 
+  //   component:LoginComponent
+  // },
+
+  {
+    path: '',
+    loadChildren: () => import('./features/vehicles-dashboard/vehicles-dashboard.module')
+    .then(m => m.VehiclesDashboardModule)
+  },
+  // {
+  //   path: '',
+  //   loadChildren: () => import('./features/category-dashboard/category-selection/category-selection.component')
+  //   .then(m => m.CategorySelectionComponent)
+  // },
+];
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(routes, { useHash: true })
+  ],
+  bootstrap: [AppComponent],
+  //imports: [RouterModule.forRoot(routes, { useHash: true })],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
+
+//export const routing = RouterModule.forRoot(routes);
